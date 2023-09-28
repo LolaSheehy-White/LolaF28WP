5b2e225fe56334d3d272eb91c137af0a08999630
LolaSheehy-White <144949247+LolaSheehy-White@users.noreply.github.com>
2023-09-28 10:35:28 +0100

    altering spelling and code

---
+++ javascript_testing/javasc.js
@@ -0,0 +1,12 @@
+/*<!DOCTYPE Javascript>
+<jsconfig.json> </jsconfig.json>*/
+var HTMLBodyElement:{
+    var today = new Date();
+        var day = today.getDay();
+        var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+        console.log("Today is : " + daylist[day] + ".");
+        var hour = today.getHours();
+        var minute = today.getMinutes();
+        var second = today.getSeconds();
+}
+
